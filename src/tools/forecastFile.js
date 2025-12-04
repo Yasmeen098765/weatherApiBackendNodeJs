2cc93732acd7e20bb91316b0983c67f161ba4c78
yasmeen mamdouh sharaf eddin <nnanahamoud123@gmail.com>
2025-12-04 23:58:53 +0300

    Add files via upload

---
+++ src/tools/forecastFile.js
@@ -0,0 +1,22 @@
+const request = require('request')
+const forecast = (latitude,longitude,callback) =>{
+    const url = "https://api.weatherapi.com/v1/current.json?key=51a20bc2793045b6b26142621220803&q=" + latitude + ',' + longitude
+       request({url,json:true},(error,response)=>{
+        // low level error 
+        if(error){
+            callback('Unable to connect weather service',undefined)
+        }
+        else if(response.body.error){
+           callback(response.body.error.message,undefined)
+        }
+        else {
+           callback(undefined,
+            {location:response.body.location.name,
+            weather_condition:response.body.current.condition.text,
+             temp_c:response.body.current.temp_c
+            }
+        )
+        }
+    }) 
+}
+module.exports = forecast
